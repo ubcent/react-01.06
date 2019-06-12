b4707fedd19588112668b3d0197337a36daffd32
dvbondarchuk@gmail.com <dvbondarchuk@gmail.com>
2019-06-13 00:59:44 +0300

    Back-end kick-off

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const pictures = require('./pictures');ß
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/api/photos', (req, res) => {
+  res.json(pictures);
+});
+
+app.listen(8888, () => {
+  console.log('Server has been started!');
+});
